3ed2a44c0b390195c7a34349cd07fc5cf6e4de21
Eduard1q2w <edithehigh@gmail.com>
2022-02-04 16:53:59 +0200

    toate fisierele

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express'),
+	app = express();
+const path = require('path');
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, 'views/pages', 'index.html'));
+});
+
+
+app.get('/blog', function(req, res) {
+	res.sendFile(path.join(__dirname, 'views/pages', 'blog.html'));
+});
+app.get('/gallery', function(req, res) {
+	res.sendFile(path.join(__dirname, 'views/pages', 'gallery.html'));
+});
+
+
+var port = process.env.PORT || 3000;
+app.listen(port)
+console.log('Server is listening on port', port)
